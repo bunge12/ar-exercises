4fdc379711f34bd7ec1e72c31103bcfe66a4c6e1
Artur Iatsko <arturiatsko@Arturs-MBP.hitronhub.home>
2020-03-30 17:21:38 -0700

    Where statements (ex4)

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,22 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+store4 = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+store5 = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+store6 = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+puts "Mens stores:"
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each { |store| puts "Store: #{store.name}, revenue: #{store.annual_revenue}"}
+
+puts "Womens stores <$1m:"
+@womens_stores = Store.where(womens_apparel: true)
+
+@womens_stores.each { |store| puts "Store: #{store.name}, revenue: #{store.annual_revenue}"}
+
+
+# - Surrey (annual_revenue of 224000, carries women's apparel only)
+# - Whistler (annual_revenue of 1900000 carries men's apparel only)
+# - Yaletown (annual_revenue of 430000 carries men's and women's apparel)
